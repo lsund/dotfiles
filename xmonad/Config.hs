987eefdb6ffa9c9d6876e19b603671ef025d67d7
Ludvig Sundström <lud.sund@gmail.com>
2017-08-25 17:34:34 +0200

    Refactor xmonad
    split PPBox config into Boxes.hs
    some cleanup/formatting

--- xmonad/Config.hs
+++ xmonad/Config.hs
@@ -2,15 +2,15 @@
 module Config where
 
 -- Looks
-dzenFont       = "xft:Source Code Pro:size=10"
-colorBlack     = "#020202"                                          --Background
-colorBlackAlt  = "#1c1c1c"                                          --Black Xdefaults
-colorGray      = "#444444"                                          --Gray
-colorGrayAlt   = "#101010"                                          --Gray dark
+dzenFont       = "xft:Source Code Pro:size=14"
+colorBlack     = "#020202"
+colorBlackAlt  = "#1c1c1c"
+colorGray      = "#444444"
+colorGrayAlt   = "#101010"
 colorGrayAlt2  = "#404040"
 colorGrayAlt3  = "#252525"
-colorWhite     = "#a9a6af"                                          --Foreground
-colorWhiteAlt  = "#9d9d9d"                                          --White dark
+colorWhite     = "#a9a6af"
+colorWhiteAlt  = "#9d9d9d"
 colorWhiteAlt2 = "#b5b3b3"
 colorWhiteAlt3 = "#707070"
 colorMagenta   = "#8e82a2"
@@ -20,13 +20,12 @@ colorRed       = "#f7a16e"
 colorRedAlt    = "#e0105f"
 colorGreen     = "#66ff66"
 colorGreenAlt  = "#558965"
-boxLeftIcon    = "/home/lsund/.icons/xbm_icons/subtle/boxleft.xbm"          --left icon of dzen boxes
-boxLeftIcon2   = "/home/lsund/.icons/xbm_icons/subtle/boxleft2.xbm"         --left icon2 of dzen boxes
-boxRightIcon   = "/home/lsund/.icons/xbm_icons/subtle/boxright.xbm"         --right icon of dzen boxes
-panelHeight    = 16 :: Int                                                  --height of top and bottom panels (16)
-boxHeight      = 14 :: Int                                                  --height of dzen logger box (14)
-topPanelSepPos = 950 :: Int                                                 --left-right alignment pos of top panel
-botPanelSepPos = 900 :: Int                                                 --left-right alignment pos of bottom panel
+boxLeftIcon2   = "/home/lsund/.icons/xbm_icons/subtle/boxleft2-big.xbm"
+boxRightIcon   = "/home/lsund/.icons/xbm_icons/subtle/boxright.xbm"
+panelHeight    = 24 :: Int
+boxHeight      = 22 :: Int
+topPanelSepPos = 950 :: Int
+botPanelSepPos = 900 :: Int
 
 myTerminal = "/usr/bin/urxvt"
 logpath = "/home/lsund/Data/log/"
