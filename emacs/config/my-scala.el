4fa34b845dc97fbcb87b73c0d3f0d0065cd6c152
Ludvig Sundström <lud.sund@gmail.com>
2019-05-31 16:27:22 +0200

    Add my-scala

---
+++ emacs/config/my-scala.el
@@ -0,0 +1,17 @@
+;;; package --- Summary
+
+
+;; Scala specific config
+
+;;; Commentary:
+
+;;; Code:
+
+(provide 'my-scala)
+
+(require-packages '(ensime scala-mode))
+
+(add-to-list 'exec-path "/usr/local/bin")
+
+
+;;; my-scala.el ends here
